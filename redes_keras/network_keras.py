2af4abe83de95b7d0c0be678868a4d303e75623a
José Ramón <gjose3159@gmail.com>
2024-02-11 15:07:52 -0600

    Se cambió el modelo al de la tarea2, y se corrió, se notó el mismo tiempo pero con mejores resultados

--- redes_keras/network_keras.py
+++ redes_keras/network_keras.py
@@ -1,61 +1,62 @@
-#https://www.tutorialspoint.com/keras/keras_installation.htm
-#https://docs.python.org/es/3/tutorial/venv.html
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.optimizers import RMSprop, SGD
-from keras.callbacks import ModelCheckpoint, EarlyStopping #Para guardar la red y para detener la red cuando ya sobreajusta.
+from tensorflow.keras.optimizers import RMSprop, SGD, Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping #Guarda la mejor red
 import matplotlib.pyplot as plt
-learning_rate = 0.001
-epochs = 30
-batch_size = 120
-###################
-#pip install -q mlflow
 import mlflow
-#Servidor que guarda el historial de entrenamieto, guarda el modelo y los parámetros. Para ver el más chido.
-mlflow.tensorflow.autolog() #Empezamos a activar el servidor
-dataset=mnist.load_data() #Cargamos los datos
-
-(x_train, y_train), (x_test, y_test) = dataset #Se separan por entrenamiento y prueba
-#print(y_train.shape)
-#print(x_train.shape)
-#print(x_test.shape)
-#x_train=x_train[0:8000]
-#x_test=x_train[0:1000]
-#y_train=y_train[0:8000]
-#y_test=y_train[0:1000]
-x_trainv = x_train.reshape(60000, 784)
-x_testv = x_test.reshape(10000, 784)
-#print(x_trainv[3])
-x_trainv = x_trainv.astype('float32')
-x_testv = x_testv.astype('float32')
-x_trainv /= 255  # x_trainv = x_trainv/255
-x_testv /= 255
-#print("linea 40--------")
-#print(x_trainv[3])
-#print(x_train.shape)
-#print(x_trainv.shape)
-num_classes=10
+#------------------------------------Parámetros-----------------------------------------------
+learning_rate = 0.05
+epochs = 30
+batch_size = 10
+beta_1 = 0.9
+beta_2 = 0.999
+epsilon= 1e-8
+modelo = 'Adam'
+#------------------------------------Datos del modelo-----------------------------------------
+#Activa el servidor
+mlflow.tensorflow.autolog()    
+#Cargamos los datos
+dataset  = mnist.load_data()   
+(x_train, y_train), (x_test, y_test) = dataset
+#Aplanamos a las imagenes
+x_trainv = x_train.reshape(60000, 784).astype('float32') 
+x_testv  = x_test.reshape(10000, 784).astype('float32')
+#Normalizamos los datos
+x_trainv /= 255 
+x_testv /= 255 
+#One-hot encoding
+num_classes=10 
 y_trainc = keras.utils.to_categorical(y_train, num_classes)
 y_testc = keras.utils.to_categorical(y_test, num_classes)
-#print(y_trainc[6:15])
+#------------------------------------Modelo-----------------------------------------
 model = Sequential()
-model.add(Dense(512, activation='sigmoid', input_shape=(784,)))
+#Capas
+model.add(Dense(30, activation='sigmoid', input_shape=(784,)))
 #model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu'))
+#model.add(Dense(512, activation='relu'))
 #model.add(Dense(num_classes, activation='softmax'))
 model.add(Dense(num_classes, activation='sigmoid'))
 model.summary()
 ####Checkpoint and EarlyStopping#####
-filepath = "best_model.hdf5" #specify path to save model
+#specify path to save model
+filepath = "best_model.hdf5" 
 #Checkpoint to save the model
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min') #el 'min' de 'val_loss'
+checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max') #el 'min' de 'val_loss'
 #Early stopping
-earlystop = EarlyStopping(monitor='val_loss',mode='min',restore_best_weights=True,patience=10,verbose=1) # verbose =
-model.compile(loss='categorical_crossentropy',optimizer=SGD(learning_rate=learning_rate),
-                metrics=['accuracy'])
+earlystop = EarlyStopping(monitor='val_loss',mode='min',restore_best_weights=True,patience=10,verbose=1)
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=Adam(
+        learning_rate=learning_rate,
+        beta_1=beta_1,
+        beta_2=beta_2,
+        epsilon=epsilon,
+        amsgrad=True 
+    ),
+    metrics=['accuracy'])
 history = model.fit(x_trainv, y_trainc,
                     batch_size=batch_size,
                     epochs=epochs,
@@ -63,10 +64,22 @@ history = model.fit(x_trainv, y_trainc,
                     validation_data=(x_testv, y_testc),
                     callbacks=[checkpoint,earlystop]
                     )
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.figure()
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
+#------------------Gráficas---------------------------------
+fig, ax=plt.subplots(1,2, figsize=(20,10))
+ax[0].set_title(f'loss vs epoch,beta_1={beta_1},beta_2={beta_2},eta={learning_rate}')
+ax[0].plot(history.history['loss'],'r', label='loss')
+ax[0].plot(history.history['val_loss'],'b', label='val_loss')
+ax[0].set_ylabel('loss')
+ax[0].set_xlabel('epoch')
+ax[0].legend()
+ax[1].set_title('accuracy vs epoch')
+ax[1].plot(history.history['accuracy'],'r', label='accuracy')
+ax[1].plot(history.history['val_accuracy'],'b', label='val_accuracy')
+ax[1].set_ylabel('loss')
+ax[1].set_xlabel('epoch')
+ax[1].legend()
+
+fig.tight_layout()
+plt.savefig(f"Modelo:{modelo}.jpg")
 plt.show()
 #en consola: mlflow ui
