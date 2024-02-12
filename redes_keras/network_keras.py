41ca84ff944a95a6f946f0832fc906bf2ed11096
José Ramón <gjose3159@gmail.com>
2024-02-11 19:13:23 -0600

    Experimento numero 2, revertido

--- redes_keras/network_keras.py
+++ redes_keras/network_keras.py
@@ -11,8 +11,8 @@ import mlflow
 learning_rate = 0.001
 epochs = 30
 batch_size = 10
-beta_1 = 0.9
-beta_2 = 0.999
+beta_1 = 0.09
+beta_2 = 0.9999
 epsilon= 1e-7
 modelo = 'Adam'
 #------------------------------------Datos del modelo-----------------------------------------
@@ -34,14 +34,9 @@ y_testc = keras.utils.to_categorical(y_test, num_classes)
 #------------------------------------Modelo-----------------------------------------
 model = Sequential()
 #Capas
-model.add(Dense(300, activation='sigmoid', input_shape=(784,)))
+model.add(Dense(200, activation='sigmoid', input_shape=(784,)))
 #model.add(Dropout(0.2))
-<<<<<<< HEAD
-model.add(Dense(200, activation='sigmoid'))
 model.add(Dense(100, activation='relu'))
-=======
-#model.add(Dense(512, activation='relu'))
->>>>>>> parent of 600601e (Experimento numero 2)
 #model.add(Dense(num_classes, activation='softmax'))
 model.add(Dense(num_classes, activation='sigmoid'))
 model.summary()
